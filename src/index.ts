5466ee0d69be5dadf73cc9d1f09ba23c89cd8bf1
TheOrlok <rodolfo_popmundo@hotmail.com>
2021-08-11 13:58:49 -0300

    Refresh Token implementation

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,22 @@ import { createConnection } from "typeorm"
 
 async function connect() {
     const app = express()
-    await createConnection()
+    //Database connection
+    try {
+        await createConnection()
+    } catch (error) {
+        throw new Error('Could not connect to database')
+    }
     app.use('/health_check', (_, res) => res.sendStatus(200))
-    app.use('/graphql', graphqlHTTP({
+    app.use('/graphql', graphqlHTTP(async (req, res) => ({
         schema: await buildSchema({
             resolvers: [UserResolver]
         }),
-      }))
+        context: {
+            req,
+            res,
+        }
+      })))
     app.listen(3000)
     console.log(`🚀 Server ready at http://localhost:3000`)
     return { app }
--- src/resolver/UserResolver.ts
+++ src/resolver/UserResolver.ts
@@ -1,7 +1,16 @@
+import { Response, Request } from 'express'
 import { User } from "../entity/User"
-import { Arg, Field, Mutation, ObjectType, Query, Resolver } from "type-graphql"
+import { Arg, Ctx, Field, Mutation, ObjectType, Query, Resolver } from "type-graphql"
 import { compare, hash } from 'bcryptjs'
 import { sign } from "jsonwebtoken"
+
+interface Context {
+    req: Request,
+    res: Response
+}
+
+const createAccessToken = (userId: number) => sign({ userId }, 'asdfasdfsdjfh', { expiresIn: '3m' })
+const createRefreshToken = (userId: number) => sign({ userId }, 'okkookoijinooihdsfa', { expiresIn: '7d' })
 
 @ObjectType()
 class LoginResponse {
@@ -19,7 +28,8 @@ export class UserResolver {
     @Mutation(() => LoginResponse)
     async login(
         @Arg('email') email: string,
-        @Arg('password') password: string
+        @Arg('password') password: string,
+        @Ctx() { res }: Context
     ): Promise<LoginResponse> {
         const user = await User.findOne({ where: { email } })
 
@@ -33,8 +43,14 @@ export class UserResolver {
             throw new Error('invalid credentials')
         }
 
+        //login success
+
+        res.cookie('jid', createRefreshToken(user.id), {
+            httpOnly: true,
+        })
+
         return {
-            accessToken: sign({ userId: user.id }, 'asdfasdfsdjfh', { expiresIn: '3m' })
+            accessToken: createAccessToken(user.id)
         }
     }
 
